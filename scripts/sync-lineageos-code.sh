ef7ec268ae7af2ebd7241ffcefaf0db3fa65a462
Jimmy Wu <jimmy.wu@richash.co>
2020-01-23 05:53:40 +0800

    加入LINEAGEOS_DIR環境變數

--- scripts/sync-lineageos-code.sh
+++ scripts/sync-lineageos-code.sh
@@ -12,6 +12,18 @@ add_path_env(){
     fi
 }
 
+add_lineageOS_folder_env(){
+    NEW_PATH=$1
+    if grep -Fxq "export LINEAGEOS_DIR=$NEW_PATH" $PROFILE_PATH
+    then
+        echo "environment variable LINEAGEOS_DIR already exites, values is $LINEAGEOS_DIR"
+    else
+        echo "export LINEAGEOS_DIR=$NEW_PATH" >> $PROFILE_PATH
+        source $PROFILE_PATH
+        echo "environment variable LINEAGEOS_DIR not exites, add with values $LINEAGEOS_DIR"
+    fi
+}
+
 
 BIN_DIR=$HOME/bin
 REPO_PATH=$BIN_DIR/repo
@@ -28,25 +40,33 @@ read_var_frm_input(){
     fi
     #echo $USER_INPUT
 }
-
+# 指定lineageOS程式碼目錄
+# ===========================================================
 PROMPT_MSG="Please entry lineageOS checkout folder"
 DEFAULT_VAL="$HOME/lineageOS"
 read_var_frm_input "${PROMPT_MSG}" "${DEFAULT_VAL}"
 LINEAGEOS_DIR=$USER_INPUT
-echo "lineageOS worker forder is $LINEAGEOS_DIR" 
+echo "lineageOS checkout forder is $LINEAGEOS_DIR" 
+add_lineageOS_folder_env $LINEAGEOS_DIR
 
+# 指定lineageOS 分支版本
+# ===========================================================
 PROMPT_MSG="Please entry checkout lineageOS branch"
 DEFAULT_VAL="lineage-15.1"
 read_var_frm_input "${PROMPT_MSG}" "${DEFAULT_VAL}"
 GIT_BRANCH=$USER_INPUT
 echo "lineageOS branch is $GIT_BRANCH"
 
+# 設定 git global user.name
+# ===========================================================
 PROMPT_MSG="Please entry your git global user.name"
 DEFAULT_VAL="Your Name"
 read_var_frm_input "${PROMPT_MSG}" "${DEFAULT_VAL}"
 GIT_USERNAME=$USER_INPUT
 echo "git global user.name=$GIT_USERNAME" 
 
+# 設定 git global user.email
+# ===========================================================
 PROMPT_MSG="Please entry your git global user.email"
 DEFAULT_VAL="you@example.com"
 read_var_frm_input "${PROMPT_MSG}" "${DEFAULT_VAL}"
@@ -54,6 +74,8 @@ GIT_USEREMAIL=$USER_INPUT
 echo "git global user.email=$GIT_USEREMAIL"
 
 
+# 下載Repo程式碼管理工具
+# ===========================================================
 if [ ! -d $BIN_DIR ] 
 then
     mkdir $BIN_DIR
@@ -64,25 +86,35 @@ else
     echo "folder already exits. $BIN_DIR"
 fi
 
+# 建立lineageOS程式碼目錄
+# ===========================================================
 if [ ! -d $LINEAGEOS_DIR ] 
 then
     mkdir $LINEAGEOS_DIR
-    cd $LINEAGEOS_DIR
-    if [ -z $(git config --global user.name) ] 
-    then
-        git config --global user.name "$GIT_USER_NAME"        
-    fi
-    echo "your git global user.name is $(git config --global user.name)"
-
-    if [ -z $(git config --global user.email) ] 
-    then
-        git config --global user.email "$GIT_USER_EMAIL"
-    fi
-    echo "your git global user.email is $(git config --global user.email)"
-
-    repo init -u git://github.com/LineageOS/android.git -b $GIT_BRANCH
-    repo sync -j32
-else
-    echo "folder already exits. $LINEAGEOS_DIR"
-    exit 1
 fi
+
+
+# 設定 git global user.name
+# ===========================================================
+cd $LINEAGEOS_DIR
+if [ -z $(git config --global user.name) ] 
+then
+    git config --global user.name "$GIT_USER_NAME"        
+fi
+echo "your git global user.name is $(git config --global user.name)"
+
+# 設定 git global user.email
+# ===========================================================
+if [ -z $(git config --global user.email) ] 
+then
+    git config --global user.email "$GIT_USER_EMAIL"
+fi
+echo "your git global user.email is $(git config --global user.email)"
+
+# 初始化repository的分支
+# ===========================================================
+repo init -u git://github.com/LineageOS/android.git -b $GIT_BRANCH
+
+# 開始下載程式碼
+# ===========================================================
+repo sync -j32
